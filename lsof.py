5057a60b21eb6f5303b880c58c1fdef61f54bbd1
RithikaThirumeni <60179445+RithikaThirumeni@users.noreply.github.com>
2021-11-25 09:49:54 +0530

    lsof changed -- can find .txt files

---
+++ lsof.py
@@ -0,0 +1,42 @@
+import psutil
+import os
+import sys
+import magic
+def if_not_exists(dir_path):
+    if(os.path.exists(dir_path) == False):
+        print('Cannot open \'' + dir_path + '\' (No such file or directory)')
+        sys.exit()
+def is_open_file(fpath):
+    for proc in psutil.process_iter():
+        try:
+            for item in proc.open_files():
+                procs_info = proc.as_dict(attrs = ['pid','name','cpu_percent', 'cmdline'])
+                if fpath == item.path:
+                    if psutil.Process()==proc: print("yes")
+                    return proc
+                elif fpath in procs_info['cmdline']:
+                    return proc
+        except Exception:
+            pass
+    return False
+
+# base_path = '/home/rithika/Downloads/'
+def lsof(dir_path):
+    if_not_exists(dir_path)
+    entries = os.listdir(dir_path)
+    for entry in entries:
+        p = is_open_file(dir_path+entry)
+        if not p == False:
+            print(entry+' \n--Opened by process = '+p.name()+'\n--PID = '+str(p.pid)+'\n')
+
+# if(sys.argv[1] == '--help'):
+#     f = open('help_files/help_lsof.txt', 'r')
+#     print(f.read())
+# elif(sys.argv[1] == "man"):
+#     f = open('man_files/man_lsof.txt', 'r')
+#     print(f.read())
+# else:
+#     lsof(sys.argv[1])
+print(psutil.Process().cmdline())
+lsof("/home/rithika/Downloads/sea-shell/")
+
